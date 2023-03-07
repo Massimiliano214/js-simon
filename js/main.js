859ae3f544790a3ed89b19e77a740933a624b94a
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-07 15:21:35 +0100

    impostazione timeouts

---
+++ js/main.js
@@ -0,0 +1,35 @@
+containerDom = document.querySelector(".container");
+
+containerDom.innerHTML
+
+let addNum = randomNum();
+
+for (i = 0; i < 5; i++) {
+    let addNum = randomNum();
+    addNum.innerHTML += randomNum();
+    console.log(addNum);
+    containerDom.innerHTML +=" " + randomNum() + ";";
+}
+
+
+
+function randomNum() {
+    let numGot = Math.floor(Math.random() * 20) + 1;
+    return numGot;
+}
+
+
+setTimeout(function() {
+    containerDom.innerHTML = "?";
+}, 1000);
+
+
+setTimeout(function() {
+    const primoInput = prompt("Inserisci il primo numero");
+    const secondoInput = prompt("Inserisci il secondo numero");
+    const terzoInput = prompt("Inserisci il terzo numero");
+    const quartoInput = prompt("Inserisci il quarto numero");
+    const quintoInput = prompt("Inserisci il quinto numero");
+
+    console.log(primoInput, secondoInput, terzoInput, quartoInput, quintoInput);
+}, 1100);
